ef459dbe2b0d736007fe0588c1f480bb7c54e1bb
王聪 <wangcong@wujiangdeMac-mini.local>
2015-12-03 12:06:23 +0800

    fdssd

---
+++ CloudPhone/Views/DailNumberCell.h
@@ -0,0 +1,19 @@
+//
+//  DailNumberCell.h
+//  CloudPhone
+//
+//  Created by wangcong on 15/12/3.
+//  Copyright © 2015年 iTal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DailNumberCell : UITableViewCell
+
+@property (nonatomic, strong) UIImageView *dailImageView;
+
+@property (nonatomic, strong) UILabel *dailNameLabel;
+
+@property (nonatomic, strong) UILabel *dailNumberLabel;
+
+@end
