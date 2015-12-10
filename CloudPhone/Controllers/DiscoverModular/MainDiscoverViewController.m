7c22ef70a62c4be7da5ae67e1a21e62b2d883eb4
iTelDeng <iteldeng@MinideMac-mini.local>
2015-12-10 10:11:15 +0800

    1209taskcenter

--- CloudPhone/Controllers/DiscoverModular/MainDiscoverViewController.m
+++ CloudPhone/Controllers/DiscoverModular/MainDiscoverViewController.m
@@ -7,31 +7,185 @@
 //
 
 #import "MainDiscoverViewController.h"
-
-@interface MainDiscoverViewController ()
-
+#import "Global.h"
+@interface MainDiscoverViewController ()<UIScrollViewDelegate>
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIPageControl *pageControl;
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, strong) NSArray *imagesArray;
 @end
 
 @implementation MainDiscoverViewController
+- (NSArray *)imagesArray{
+    if (!_imagesArray) {
+        _imagesArray = [NSArray array];
+        _imagesArray = @[@"find_adv01",@"find_adv02",@"find_adv03"];
+    }
+    return _imagesArray;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setupScrollView];
+    [self setupPageControl];
+    [self setupAppBar];
+}
+
+- (void)setupScrollView {
+    CGFloat scrollViewX = 0;
+    CGFloat scrollViewY = STATUS_NAV_BAR_HEIGHT;
+    CGFloat scrollViewH = 140;
+    CGFloat scrollViewW = MainWidth;
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(scrollViewX, scrollViewY, scrollViewW, scrollViewH)];
+    scrollView.showsHorizontalScrollIndicator = NO;
+    scrollView.contentSize = CGSizeMake(self.imagesArray.count * scrollViewW, -1);
+    scrollView.pagingEnabled = YES;
+    self.scrollView = scrollView;
+    self.scrollView.delegate = self;
+    [self setupImageView];
+    [self.view addSubview:scrollView];
+    [self addTimer];
+}
+
+- (void)setupPageControl{
+    UIPageControl *pageControl = [[UIPageControl alloc] init];
+    [self.view addSubview:pageControl];
+    pageControl.numberOfPages = self.imagesArray.count;
+    pageControl.pageIndicatorTintColor = [UIColor lightGrayColor];
+    pageControl.currentPageIndicatorTintColor = [UIColor whiteColor];
+    CGFloat pageControlY = self.scrollView.frame.size.height + self.scrollView.frame.origin.y - 20;
+    pageControl.center = CGPointMake(self.view.frame.size.width / 2, pageControlY);
+    self.pageControl = pageControl;
+}
+
+- (void)setupImageView{
+    CGFloat imageW = MainWidth;
+    CGFloat imageH = self.scrollView.frame.size.height;
+    CGFloat imageY = 0;
+    // 添加图片
+    for (int i = 0; i < self.imagesArray.count; i++) {
+        CGFloat imageX = i * imageW;
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(imageX, imageY, imageW, imageH)];
+        [imageView setImage:[UIImage imageNamed:self.imagesArray[i]]];
+        [self.scrollView addSubview:imageView];
+    }
+}
+
+//scrollView滚动时调用
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGFloat scrollviewW =  scrollView.frame.size.width;
+    CGFloat x = scrollView.contentOffset.x;
+    int page = (x + scrollviewW / 2) /  scrollviewW;
+    self.pageControl.currentPage = page;
+}
+// 开始拖拽的时候调用
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    [self removeTimer];
+}
+//拖拽结束调用
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+    [self addTimer];
+}
+
+- (void)nextImage{
+    int page = (int)self.pageControl.currentPage;
+    if (page == self.imagesArray.count - 1) {
+        page = 0;
+    }
+    else{
+        page++;
+    }
+    CGFloat x = page * self.scrollView.frame.size.width;
+    self.scrollView.contentOffset = CGPointMake(x, 0);
+}
+//开启定时器
+- (void)addTimer{
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(nextImage) userInfo:nil repeats:YES];
+}
+//关闭定时器
+- (void)removeTimer{
+    [self.timer invalidate];
+}
+
+- (void)setupAppBar{
+    
+    NSArray *imageArray = [NSArray array];
+    imageArray = @[@"find_miaomiao",@"find_tips",@"find_takeout",@"find_bank",@"find_flight",@"find_film",@"find_add"];
+    NSArray *nameArray = [NSArray array];
+    nameArray = @[@"瞄瞄购",@"小费",@"外卖",@"银行",@"航班",@"电影",@" "];
+    
+    UIScrollView *appBar= [[UIScrollView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.scrollView.frame), MainWidth, 300)];
+    appBar.contentSize = CGSizeMake(0, 500);
+    [self.view addSubview:appBar];
+
+    
+    for (int i = 0; i < imageArray.count; i++) {
+        int col = i % 3;
+        int row = i / 3;
+        CGFloat  btWidth = (MainWidth - 25 * 2 - 15 * 2) / 3.0;
+        CGFloat  btHeight = btWidth;
+        CGFloat  btX = 25 + col * (btWidth +15);
+        CGFloat  btY = 15 + row * (btWidth + 20 + 10);
+        
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = CGRectMake(btX, btY, btWidth, btHeight);
+        [button setImage:[UIImage imageNamed:[NSString stringWithFormat:@"%@",imageArray[i]]] withTitle:[NSString stringWithFormat:@"%@",nameArray[i]] forState:UIControlStateNormal];
+        button.tag = i;
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [button.titleLabel setTextAlignment:NSTextAlignmentCenter];
+        [button addTarget:self action:@selector(clickButton:) forControlEvents:UIControlEventTouchUpInside];
+        [appBar addSubview:button];
+   }
+}
+
+
+- (void)clickButton:(UIButton *)sender{
+    switch (sender.tag) {
+        case 0:{
+            //静音
+            DLog(@"0");
+        }
+            break;
+            
+        case 1:{
+            //免提
+            DLog(@"1");
+        }
+            break;
+            
+        case 2:{
+            //录音
+            DLog(@"2");
+        }
+            break;
+            
+        case 3:{
+            //回拨
+            DLog(@"3");
+        }
+            break;
+            
+        case 4:{
+            //挂断
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }
+            break;
+            
+        case 5:{
+            //键盘
+            DLog(@"5");
+        }
+            break;
+            
+        default:
+            break;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- CloudPhone/Controllers/MineModular/TaskCenterViewController.m
+++ CloudPhone/Controllers/MineModular/TaskCenterViewController.m
@@ -8,6 +8,10 @@
 
 #import "TaskCenterViewController.h"
 #import "Global.h"
+@interface TaskCenterViewController()<UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
+@end
 
 @implementation TaskCenterViewController
 
@@ -15,14 +19,150 @@
     [super viewDidLoad];
     self.view.backgroundColor = [ColorTool backgroundColor];
     self.title = @"任务中心";
+    [self.view addSubview:self.tableView];
     //返回
     UIButton *backButton = [self setBackBarButton:1];
     [backButton addTarget:self action:@selector(popViewController) forControlEvents:UIControlEventTouchUpInside];
     [self setBackBarButtonItem:backButton];
     
+    UIImageView *headerView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, MainWidth,140)];
+    UIImage *image = [UIImage imageNamed:@"find_adv01"];
+    headerView.contentMode = UIViewContentModeCenter;
+    headerView.image = image;
+    self.tableView.tableHeaderView = headerView;
+    
+
     
 }
 
+- (UITableView *)tableView {
+    if (!_tableView) {
+        CGRect tableViewFrame = CGRectMake(0, 0, MainWidth, SCREEN_HEIGHT);
+        _tableView = [[UITableView alloc]initWithFrame:tableViewFrame style:UITableViewStyleGrouped];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.backgroundColor = [ColorTool backgroundColor];
+    }
+    return _tableView;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section == 1) {
+        return 2;
+    }
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *ID = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:ID];
+        if (indexPath.section == 1 && indexPath.row == 1) {
+            NSArray *imageArray = [NSArray array];
+            imageArray = @[@"mine_penyouquan",@"mine_qqspace",@"mine_qq",@"mine_weixin"];
+            NSArray *nameArray = [NSArray array];
+            nameArray = @[@"盆友圈",@"空间",@"QQ",@"微信"];
+            
+            UIView *shareBar= [[UIView alloc] initWithFrame:CGRectMake(0, 0, MainWidth, 120)];
+            shareBar.backgroundColor = [UIColor whiteColor];
+            [cell addSubview:shareBar];
+            
+            for (int i = 0; i < imageArray.count; i++) {
+                int col = i % 2;
+                int row = i / 2;
+                CGFloat  btWidth = 40;
+                CGFloat  btHeight = 40.0;
+                CGFloat  btX = col ? MainWidth - 89 - btWidth : 34 - 15;
+                CGFloat  btY = 10 + row * (btHeight + 20);
+                
+                UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+                button.frame = CGRectMake(btX, btY, btWidth, btHeight);
+                [button setImage:[UIImage imageNamed:[NSString stringWithFormat:@"%@",imageArray[i]]] forState:UIControlStateNormal];
+                [button addTarget:self action:@selector(clickButton:) forControlEvents:UIControlEventTouchUpInside];
+                [shareBar addSubview:button];
+            }
+        }
+    }
+     
+    
+    if (indexPath.section == 0 ) {
+    cell.textLabel.text = @"登录签到每次奖励5分钟";
+    cell.accessoryView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"mine_arrow"]];
+    } else  if (indexPath.section == 1 && indexPath.row == 0){
+    cell.textLabel.text = @"分享云电话，奖励30分钟";
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(MainWidth - 60, 10, 40, 30)];
+    label.textColor = [UIColor colorWithHexString:@"#049ff1"];
+    label.text = @"说明";
+    cell.accessoryView = label;
+    }
+    return cell;
+}
+
+
+
+#pragma mark - UITableviewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 10;
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 1 && indexPath.row == 1) {
+        return 120;
+    }
+    return 45;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSLog(@"%ld",indexPath.row);
+    
+}
+
+
+- (void)clickButton:(UIButton *)sender{
+    switch (sender.tag) {
+        case 0:{
+            //静音
+            DLog(@"0");
+        }
+            break;
+            
+        case 1:{
+            //免提
+            DLog(@"1");
+        }
+            break;
+            
+        case 2:{
+            //录音
+            DLog(@"2");
+        }
+            break;
+            
+        case 3:{
+            //回拨
+            DLog(@"3");
+        }
+            break;            
+        default:
+            break;
+    }
+    
+}
 
 - (void)popViewController {
 
