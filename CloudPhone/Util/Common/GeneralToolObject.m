7063297a07625b36df4beb2eab7337bc189baabe
iTelDeng <iteldeng@MinideMac-mini.local>
2015-12-15 17:57:45 +0800

    20121215night

--- CloudPhone/Util/Common/GeneralToolObject.m
+++ CloudPhone/Util/Common/GeneralToolObject.m
@@ -197,6 +197,19 @@
     return (AppDelegate *)[UIApplication sharedApplication].delegate;
 }
 
-
++ (UIImage *)imageWithColor:(UIColor *)color
+{
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
 
 @end
