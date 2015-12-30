9be98146fced9ba43e98824a389ea8c2320fb240
iTelDeng <iteldeng@MinideMac-mini.local>
2015-12-30 09:28:21 +0800

    20151229night

---
+++ CloudPhone/Controllers/PhoneModular/EndDialingViewController.h
@@ -0,0 +1,13 @@
+//
+//  EndDialingViewController.h
+//  CloudPhone
+//
+//  Created by iTelDeng on 15/12/29.
+//  Copyright © 2015年 iTal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EndDialingViewController : UIViewController
+
+@end
